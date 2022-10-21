59ca66195d0ac07d7113f0fca1b9756bc5244138
conno <baush.c@northeastern.edu>
2022-10-21 18:13:55 -0400

    feat: edited function description for equipment search

--- app/src/main/java/com/example/groupwork/SearchActivity.java
+++ app/src/main/java/com/example/groupwork/SearchActivity.java
@@ -15,18 +15,25 @@ public class SearchActivity extends AppCompatActivity {
     // Choosing the option to search for equipment
     //todo actually implement this
     public void selectEquipment(android.view.View view){
+        // makes an additional drop-down list available to further specify an item category
+        //e.g. weapons, armor, tools, land vehicles, water vehicles, etc
+        // if no choice of category, user must enter a search term in the bar
+        // if category is chosen, request from web service: /api/equipment-categories/<category>
+        //this will automatically display a list of items in that category, clicking on one
+        //would open its description just as if you had searched for it
+
         // sends request to web service: /api/equipment/<search-term>
+        //<search-term> is whatever is entered into the search bar
+        //should be an exact match
         /*NOTE: search index is always lowercase
           NOTE: Game objects with a space in their name (e.g. Leather Armor)
           are written with a hyphen (e.g. studded-leather)
+          -either we handle this ourselves or instruct the user to do so
 
          */
 
-        // makes an additional drop-down list available to further specify an item category
-        //e.g. weapons, armor, tools, land vehicles, water vehicles, etc
-        // if no choice of category, stick with /api/equipment/<search-term>
-        // if category is chosen, request /api/equipment-categories/<category>
-        //this will automatically display a list of items in that category, clicking on one
-        //would open its description just as if you had searched for it
+
     }
+
+
 }
