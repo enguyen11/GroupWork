7c5e1ddb283e32ed006d980e56c6a6deb8241a20
Alan Garcia <alan_fgc@outlook.com>
2022-10-21 18:54:54 -0400

    Laid some of the classes and methods needed to create the api call and use different types of queries.

---
+++ app/src/main/java/com/example/groupwork/DndAPIActivity.java
@@ -0,0 +1,35 @@
+package com.example.groupwork;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import Dnd5ApiCaller.Dnd5Item;
+
+/**
+ * This class is the main driver for the DndMain activity.
+ */
+public class DndAPIActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private RecyclerView recyclerView;
+    private List<Dnd5Item> itemList;
+
+    public DndAPIActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Our recycler
+        recyclerView = new RecyclerView(this);
+
+    }
+}
