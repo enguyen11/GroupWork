4a4b8ec494237e289bb05b7755dd5bda3957192c
Hannah Rogers <rogershannah99@gmail.com>
2022-10-22 10:24:32 -0400

    feat: Main Activity UI
    Updated strings.xml and layout files to use non-hardcoded text
    Added button to navigate to Search activity

--- app/src/main/java/com/example/groupwork/MainActivity.java
+++ app/src/main/java/com/example/groupwork/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.groupwork;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnSearchActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSearchActivity = findViewById(R.id.button_search_activity);
+
+        btnSearchActivity.setOnClickListener(view -> {
+//            Intent goToSearch = new Intent(MainActivity.this, NEW ACTIVITY CLASS HERE );
+//            MainActivity.this.startActivity(goToSearch);
+        });
     }
 }
