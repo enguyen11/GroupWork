c777e83c83065f965dbb1d4e0a229b62f3bf2d00
Alan Garcia <alan_fgc@outlook.com>
2022-10-22 17:29:42 -0400

    git added spinner to main UI

--- app/src/main/java/com/example/groupwork/DndAPIActivity.java
+++ app/src/main/java/com/example/groupwork/DndAPIActivity.java
@@ -2,34 +2,56 @@ package com.example.groupwork;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import Dnd5ApiCaller.Dnd5Item;
 
 /**
  * This class is the main driver for the DndMain activity.
  */
-public class DndAPIActivity extends AppCompatActivity {
+public class DndAPIActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     private Spinner spinner;
     private RecyclerView recyclerView;
     private List<Dnd5Item> itemList;
-
-    public DndAPIActivity() {
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dnd_apiactivity);
 
-        //Our recycler
-        recyclerView = new RecyclerView(this);
+        // Set the spinner for all categories
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.DndApiOptions, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        String text;
+        text = adapterView.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
