c89beaea3e35a58ce4ca62ea2f7214afe6bcf10f
Eric Nguyen <enguyen1@worcester.edu>
2022-10-23 19:56:38 -0400

    Remaining two buttons on the main activity start new activities

---
+++ app/src/main/java/com/example/groupwork/Firebase.java
@@ -0,0 +1,14 @@
+package com.example.groupwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Firebase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase);
+    }
+}
--- app/src/main/java/com/example/groupwork/MainActivity.java
+++ app/src/main/java/com/example/groupwork/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnSearchActivity;
+    private Button btnSearchActivity, firebase, project;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +19,19 @@ public class MainActivity extends AppCompatActivity {
         btnSearchActivity = findViewById(R.id.button_search_activity);
 
         btnSearchActivity.setOnClickListener(view -> {
-            Intent goToSearch = new Intent(MainActivity.this, DndAPIActivity.class );
+            Intent goToSearch = new Intent(MainActivity.this, DndAPIActivity.class);
+            MainActivity.this.startActivity(goToSearch);
+        });
+
+        firebase = findViewById(R.id.button_firebase);
+        firebase.setOnClickListener(view -> {
+            Intent goToSearch = new Intent(MainActivity.this, Firebase.class);
+            MainActivity.this.startActivity(goToSearch);
+        });
+
+        project = findViewById(R.id.button_final);
+        project.setOnClickListener(view -> {
+            Intent goToSearch = new Intent(MainActivity.this, FinalProject.class);
             MainActivity.this.startActivity(goToSearch);
         });
     }
