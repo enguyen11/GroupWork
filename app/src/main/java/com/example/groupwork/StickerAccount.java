606e1dd766f585fd5996a9dfc71ebfdde8563661
conno <baush.c@northeastern.edu>
2022-11-04 18:44:51 -0400

    feat: added an incomplete function for placing sticker names into an array

--- app/src/main/java/com/example/groupwork/StickerAccount.java
+++ app/src/main/java/com/example/groupwork/StickerAccount.java
@@ -2,6 +2,7 @@ package com.example.groupwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -9,6 +10,10 @@ public class StickerAccount extends AppCompatActivity {
 
     private Button friendsButton;
     private Button stickersButton;
+    private double version;
+    private int numberOfStickers; // this will be tied to the version number
+    private Drawable[] stickerArray;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,10 @@ public class StickerAccount extends AppCompatActivity {
 
         friendsButton = findViewById(R.id.button_friends);
         stickersButton = findViewById(R.id.button_stickers);
+        /*
+            call function to fill stickerArray
+            should run on a new thread
+         */
 
         friendsButton.setOnClickListener(view -> {
             //Open friends activity
@@ -28,4 +37,21 @@ public class StickerAccount extends AppCompatActivity {
         });
         //We want the welcome message to actually display the username
     }
+
+    /**
+     * An incomplete function for filling an array with stickers
+     * Loop through starting from 0 to the number of drawables
+     * Query the database with that number, then add to the array at that number
+     */
+    /*
+        private void fillStickerArray(){
+            int num = 0;
+            while num < numberOfStickers{
+                String stickerName = **iterate_through_names.xml**
+                Drawable sticker = new Drawable(StickerAccount.this, stickerName);
+                stickerArray[num] =
+                num++;
+                }
+        }
+     */
 }
