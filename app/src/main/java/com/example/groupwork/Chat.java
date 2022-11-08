698efd3d92c289e6399a9027cf6e47a36b68754d
NB <nolenbelle@gmail.com>
2022-11-08 11:44:30 -0500

    feat: notifications with lights and large picture icon created

--- app/src/main/java/com/example/groupwork/Chat.java
+++ app/src/main/java/com/example/groupwork/Chat.java
@@ -1,5 +1,11 @@
 package com.example.groupwork;
 
+import static com.example.groupwork.App.CHANNEL_ID;
+
+import android.app.Notification;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +14,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,13 +25,17 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Chat extends AppCompatActivity {
 
+
     String userID;
     Button friendsButton;
+    NotificationManagerCompat notifManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sticker_account);
+
+        notifManager = NotificationManagerCompat.from(this);
 
         friendsButton = findViewById(R.id.button_friends);
 
@@ -44,6 +56,8 @@ public class Chat extends AppCompatActivity {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                sendChatNotification(view, "sticker_nolens_dog_demo", "Nolen");
+
                 EditText mEdit = (EditText) findViewById(R.id.sendToUser);
                 String friendID = mEdit.getText().toString();
 
@@ -75,6 +89,38 @@ public class Chat extends AppCompatActivity {
                 Toast.makeText(Chat.this, "Fail to send " + error, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void sendChatNotification(View v, String sticker, String sender) {
+        //Intent activityIntent = new Intent(this, Chat.class);
+        //PendingIntent contentIntent = PendingIntent.getActivity(this,
+        //       0, activityIntent, 0);
+        // might meed to build a back stack for the pending intent
+
+        int stickerID = Chat.this.getResources().getIdentifier(sticker,"drawable", Chat.this.getPackageName());
+
+        Bitmap picture = BitmapFactory.decodeResource(getResources(), stickerID);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.sticker_barbarian_demo)
+                .setContentTitle("Sticker Received!")
+                .setContentText("You got a sticker from " + sender)
+                .setLargeIcon(picture)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(picture)
+                        .bigLargeIcon(null)) // this makes the thumbnail vanish on the drag
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+        //        .setContentIntent(contentIntent) Not sure yet what we want to launch
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .setLights(Color.RED, 300, 100)
+                .setVibrate(new long[] { 0, 1000, 100, 1000, 100 })
+                .build();
+
+        notifManager.notify(1, notification);
+        // will need to change the ID peram if we want multiple notifications to show up at a time
+        // ideally, might be the id of the hash created in the db?
     }
 
 }
